05615e42328ab8909ce74292c6c0ef6da8f477cc
Shubham <krshubham21102005@gmail.com>
2026-03-28 17:42:11 +0530

    server setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//connect db
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+app.listen(process.env.PORT, async () => {
+  await connectDB();
+  console.log(`Server running on port ${process.env.PORT}`);
+});
